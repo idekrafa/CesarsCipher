0a6bc58e5971662ae41596cc0fbbb469019d501d
Rafael Ribeiro <rafaelribeiro2004@icloud.com>
2025-05-02 13:11:55 -0300

    CeasarsCipher codeacademy challenge

--- CesarsCipher/main.swift
+++ CesarsCipher/main.swift
@@ -7,5 +7,35 @@
 
 import Foundation
 
-print("Hello, World!")
+var alphabet: [Character] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
+var seecretMessage = "codeacademy"
+var message = Array(seecretMessage)
+
+for i in 0 ..< message.count {
+    for j in 0 ..< alphabet.count {
+        if message[i] == alphabet[j]{
+            message[i] = alphabet[(j+3) % 26]
+            break
+        }
+    }
+}
+
+print(message)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//print(message)
